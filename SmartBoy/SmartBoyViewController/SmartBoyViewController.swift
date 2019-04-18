2fe381e3c01db9283d515b9c61c939d62ebd058b
Prasanth pc <prasanthpc@Prasanths-Mac-mini.local>
2019-04-18 11:49:36 -0400

    login button added and pushed to smartBoyViewController

---
+++ SmartBoy/SmartBoyViewController/SmartBoyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SmartBoyViewController.swift
+//  SmartBoy
+//
+//  Created by Prasanth pc on 2019-04-18.
+//  Copyright © 2019 Prasanth pc. All rights reserved.
+//
+
+import UIKit
+
+class SmartBoyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
